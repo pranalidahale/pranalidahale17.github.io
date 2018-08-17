ea39deb6aa4c874092435c86e0679baadabc711a
Pranali Dahale <pranalidahale17@gmail.com>
2018-08-17 19:10:24 +0530

    changes

--- js/boxlayout.js
+++ js/boxlayout.js
@@ -1,13 +1,4 @@
-/**
- * boxlayout.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
+ 
 var Boxlayout = (function() {
 
     var $el = $('#bl-main'),
